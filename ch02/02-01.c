9fcb2972908c1e5c659722a427c6e7a468315e05
Ianyy1 <118426582+Ianyy1@users.noreply.github.com>
2022-11-17 23:41:45 +0800

    Create 02-01.c

---
+++ ch02/02-01.c
@@ -0,0 +1,12 @@
+#include<stdio.h>               /*一个简单的C程序*/
+int main(void)
+{
+    int num;                    /*定义一个名为num的变量*/
+    num = 1;                    /*为 num 赋一个值*/
+
+    printf("I an a simple ");    /*使用 printf() 函数*/
+    printf("computer.\n");
+    printf("My favourate number is %d because it is first.\n",num);
+
+    return 0;
+}
